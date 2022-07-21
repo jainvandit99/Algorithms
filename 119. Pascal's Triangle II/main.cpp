eb2348e9a12b18494b31b44ce13f6264a70c4c82
Vandit Jain <mailvandit.j@gmail.com>
2022-07-20 19:37:41 -0700

    Finished 119.

---
+++ 119. Pascal's Triangle II/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> cache = {1,1};
+        if(rowIndex == 0) return vector<int>{1};
+        int i = 2;
+        while(i<=rowIndex){
+            vector<int> newRow(i+1,1);
+            for(int j = 1; j<=i/2; j++)
+                newRow[j] = newRow[i-j] = cache[j-1] + cache[j];
+            cache = newRow;
+            i++;
+        }
+        return cache;
+    }
+};
