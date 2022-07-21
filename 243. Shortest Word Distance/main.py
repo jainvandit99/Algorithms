7bec8ecbc11a6c1f27fa7cebc5158a5ea089bf03
Chirag9009 <chiragvjv08@gmail.com>
2022-07-20 22:12:52 -0700

    updated solution for 243

--- 243. Shortest Word Distance/main.py
+++ 243. Shortest Word Distance/main.py
@@ -1,6 +1,9 @@
 """
 Author: Amal Matty Antony
 """
+
+# O(n) space 
+
 from collections import defaultdict
 class Solution:
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
@@ -12,4 +15,23 @@ class Solution:
             for b in indices[word2]:
                 m = min(m, abs(a-b))
         return m
-        
+
+
+"""
+Author: Chirag Vijayvargiya
+"""
+# constant space
+
+class Solution:
+    def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        idx1 = float("inf")
+        idx2 = float("-inf")
+        dist = abs(idx1-idx2)
+        for i, val in enumerate(wordsDict):
+            if val == word1: 
+                idx1 = i
+                dist = min(dist, abs(idx1-idx2))
+            if val == word2: 
+                idx2 = i
+                dist = min(dist, abs(idx1-idx2))
+        return dist
