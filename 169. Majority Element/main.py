6def398cbc7362dc2c817ea96cf1e4d8e1e43e16
Amal Matty Antony <amalmattyantony@gmail.com>
2022-07-20 21:26:16 -0700

    Update main.py

--- 169. Majority Element/main.py
+++ 169. Majority Element/main.py
@@ -6,3 +6,22 @@ from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         return Counter(nums).most_common(1)[0][0]
+    
+    
+    
+ # O(1) space solution
+"""
+Author: Amal Matty Antony
+"""
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        max_elem, f = nums[0], 0
+        for num in nums:
+            if num != max_elem:
+                f-=1
+                if f < 0:
+                    max_elem = num
+                    f = 1
+            else:
+                f+=1
+        return max_elem
